c899d62f27818e2be2ee20d43add8c485bab6a3c
Claire Clark <claire.clark.us@gmail.com>
2020-10-12 10:26:05 -0500

    first commit

---
+++ bot/base/BotState.hpp
@@ -0,0 +1,8 @@
+enum BotState {
+    Initializing, // Start of the Program
+    Connecting, //Connecting to Control
+    Driving, //Movement of Drivetrain
+    UsingTool, //Movement of Tool
+    Error, //Error Handling Ongoing
+    ShuttingDown
+}dir;
